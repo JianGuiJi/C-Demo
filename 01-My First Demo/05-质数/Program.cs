c4b74278310e6f0ec894e6a09c25159c479402e4
jijg <jijg@lizi.com>
2018-05-26 13:48:20 +0800

    暂存 第5课时
    暂存 第5课时

---
+++ 01-My First Demo/05-质数/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_质数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //概念：质数又称素数。一个大于1的自然数，除了1和它自身外，不能被其他自然数整除的数叫做质数；否则称为合数
+            //找出100内的所有素数
+            int length = 100;
+            for (int i = 0; i <= length; i++)
+            {
+                //计算规则：从2 到本身前一位 能不能正常
+                for (int j = 2; j < i; j++) {
+                    
+                }
+
+            }
+        }
+    }
+}
